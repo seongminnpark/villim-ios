46751bc8e97e379b3ddd4ad615de3974d58800d3
Seongmin Park <seongmin@Seongmins-iMac.local>
2017-07-21 06:10:51 -0400

    Add safety checks when parsing house json

--- Villim/VillimHouse.swift
+++ Villim/VillimHouse.swift
@@ -45,40 +45,40 @@ class VillimHouse {
 //    private var lockAddr : Int
 //    private var lockPc : Int
 
-    init(hosueInfo:SwiftyJSON.JSON) {
-        self.houseId = hosueInfo[VillimKeys.KEY_HOUSE_ID].intValue
-        self.houseName = hosueInfo[VillimKeys.KEY_HOUSE_NAME].stringValue
-        self.addrFull = hosueInfo[VillimKeys.KEY_ADDR_FULL].stringValue
-        self.addrSummary = hosueInfo[VillimKeys.KEY_ADDR_FULL].stringValue
-        self.addrDirection = hosueInfo[VillimKeys.KEY_ADDR_DIRECTION].stringValue
-        self.description = hosueInfo[VillimKeys.KEY_DESCRIPTION].stringValue
-        self.houseType = hosueInfo[VillimKeys.KEY_HOUSE_TYPE].intValue
-        self.numGuest = hosueInfo[VillimKeys.KEY_NUM_GUEST].intValue
-        self.numBedroom = hosueInfo[VillimKeys.KEY_NUM_BEDROOM].intValue
-        self.numBed = hosueInfo[VillimKeys.KEY_NUM_BED].intValue
-        self.numBathroom = hosueInfo[VillimKeys.KEY_NUM_BATHROOM].intValue
-        self.ratePerMonth = hosueInfo[VillimKeys.KEY_RATE_PER_MONTH].intValue
-        self.ratePerNight = hosueInfo[VillimKeys.KEY_RATE_PER_NIGHT].intValue
-        self.utilityFee = hosueInfo[VillimKeys.KEY_UTILITY_FEE].intValue
-        self.cleaningFee = hosueInfo[VillimKeys.KEY_CLEANING_FEE].intValue
-        self.latitude = hosueInfo[VillimKeys.KEY_LATITUDE].doubleValue
-        self.longitude = hosueInfo[VillimKeys.KEY_LONGITUDE].doubleValue
-        self.housePolicy = hosueInfo[VillimKeys.KEY_HOUSE_POLICY].stringValue
-        self.cancellationPolicy = hosueInfo[VillimKeys.KEY_CANCELLATION_POLICY].stringValue
+    init(houseInfo:SwiftyJSON.JSON) {
+        self.houseId = houseInfo[VillimKeys.KEY_HOUSE_ID].exists() ? houseInfo[VillimKeys.KEY_HOUSE_ID].intValue : 0
+        self.houseName = houseInfo[VillimKeys.KEY_HOUSE_NAME].exists() ? houseInfo[VillimKeys.KEY_HOUSE_NAME].stringValue : ""
+        self.addrFull = houseInfo[VillimKeys.KEY_ADDR_FULL].exists() ? houseInfo[VillimKeys.KEY_ADDR_FULL].stringValue : ""
+        self.addrSummary = houseInfo[VillimKeys.KEY_ADDR_SUMMARY].exists() ? houseInfo[VillimKeys.KEY_ADDR_SUMMARY].stringValue : ""
+        self.addrDirection = houseInfo[VillimKeys.KEY_ADDR_DIRECTION].exists() ? houseInfo[VillimKeys.KEY_ADDR_DIRECTION].stringValue : ""
+        self.description = houseInfo[VillimKeys.KEY_DESCRIPTION].exists() ? houseInfo[VillimKeys.KEY_DESCRIPTION].stringValue : ""
+        self.houseType = houseInfo[VillimKeys.KEY_HOUSE_TYPE].exists() ? houseInfo[VillimKeys.KEY_HOUSE_TYPE].intValue : 0
+        self.numGuest = houseInfo[VillimKeys.KEY_NUM_GUEST].exists() ? houseInfo[VillimKeys.KEY_NUM_GUEST].intValue : 0
+        self.numBedroom = houseInfo[VillimKeys.KEY_NUM_BEDROOM].exists() ? houseInfo[VillimKeys.KEY_NUM_BEDROOM].intValue : 0
+        self.numBed = houseInfo[VillimKeys.KEY_NUM_BED].exists() ? houseInfo[VillimKeys.KEY_NUM_BED].intValue : 0
+        self.numBathroom = houseInfo[VillimKeys.KEY_NUM_BATHROOM].exists() ? houseInfo[VillimKeys.KEY_NUM_BATHROOM].intValue : 0
+        self.ratePerMonth = houseInfo[VillimKeys.KEY_RATE_PER_MONTH].exists() ? houseInfo[VillimKeys.KEY_RATE_PER_MONTH].intValue : 0
+        self.ratePerNight = houseInfo[VillimKeys.KEY_RATE_PER_NIGHT].exists() ? houseInfo[VillimKeys.KEY_RATE_PER_NIGHT].intValue : 0
+        self.utilityFee = houseInfo[VillimKeys.KEY_UTILITY_FEE].exists() ? houseInfo[VillimKeys.KEY_UTILITY_FEE].intValue : 0
+        self.cleaningFee = houseInfo[VillimKeys.KEY_CLEANING_FEE].exists() ? houseInfo[VillimKeys.KEY_CLEANING_FEE].intValue : 0
+        self.latitude = houseInfo[VillimKeys.KEY_LATITUDE].exists() ? houseInfo[VillimKeys.KEY_LATITUDE].doubleValue : 0.0
+        self.longitude = houseInfo[VillimKeys.KEY_LONGITUDE].exists() ? houseInfo[VillimKeys.KEY_LONGITUDE].doubleValue : 0.0
+        self.housePolicy = houseInfo[VillimKeys.KEY_HOUSE_POLICY].exists() ? houseInfo[VillimKeys.KEY_HOUSE_POLICY].stringValue : ""
+        self.cancellationPolicy = houseInfo[VillimKeys.KEY_CANCELLATION_POLICY].exists() ? houseInfo[VillimKeys.KEY_CANCELLATION_POLICY].stringValue : ""
         
-        self.hostId = hosueInfo[VillimKeys.KEY_HOST_ID].intValue
-        self.hostName = hosueInfo[VillimKeys.KEY_HOST_NAME].stringValue
-        self.hostRating = hosueInfo[VillimKeys.KEY_HOST_RATING].floatValue
-        self.hostReviewCount = hosueInfo[VillimKeys.KEY_HOST_REVIEW_COUNT].intValue
-        self.hostProfilePicUrl = hosueInfo[VillimKeys.KEY_HOST_PROFILE_PIC_URL].stringValue
-        self.houseRating = hosueInfo[VillimKeys.KEY_HOUSE_RATING].floatValue
-        self.houseReviewCount = hosueInfo[VillimKeys.KEY_HOUSE_REVIEW_COUNT].intValue
+        self.hostId = houseInfo[VillimKeys.KEY_HOST_ID].exists() ? houseInfo[VillimKeys.KEY_HOST_ID].intValue : 0
+        self.hostName = houseInfo[VillimKeys.KEY_HOST_NAME].exists() ? houseInfo[VillimKeys.KEY_HOST_NAME].stringValue : ""
+        self.hostRating = houseInfo[VillimKeys.KEY_HOST_RATING].exists() ? houseInfo[VillimKeys.KEY_HOST_RATING].floatValue : 0.0
+        self.hostReviewCount = houseInfo[VillimKeys.KEY_HOST_REVIEW_COUNT].exists() ? houseInfo[VillimKeys.KEY_HOST_REVIEW_COUNT].intValue : 0
+        self.hostProfilePicUrl = houseInfo[VillimKeys.KEY_HOST_PROFILE_PIC_URL].exists() ? houseInfo[VillimKeys.KEY_HOST_PROFILE_PIC_URL].stringValue : ""
+        self.houseRating = houseInfo[VillimKeys.KEY_HOUSE_RATING].exists() ? houseInfo[VillimKeys.KEY_HOUSE_RATING].floatValue : 0.0
+        self.houseReviewCount = houseInfo[VillimKeys.KEY_HOUSE_REVIEW_COUNT].exists() ? houseInfo[VillimKeys.KEY_HOUSE_REVIEW_COUNT].intValue : 0
         
-        self.houseThumbnailUrl = hosueInfo[VillimKeys.KEY_HOUSE_THUMBNAIL_URL].stringValue
-        self.amenityIds = hosueInfo[VillimKeys.KEY_AMENITY_IDS].arrayValue.map{ $0.intValue }
-        self.housePicUrls = hosueInfo[VillimKeys.KEY_HOUSE_PIC_URLS].arrayValue.map{ $0.stringValue }
+        self.houseThumbnailUrl = houseInfo[VillimKeys.KEY_HOUSE_THUMBNAIL_URL].exists() ? houseInfo[VillimKeys.KEY_HOUSE_THUMBNAIL_URL].stringValue : ""
+        self.amenityIds = houseInfo[VillimKeys.KEY_AMENITY_IDS].exists() ? houseInfo[VillimKeys.KEY_AMENITY_IDS].arrayValue.map{ $0.intValue } : []
+        self.housePicUrls = houseInfo[VillimKeys.KEY_HOUSE_PIC_URLS].exists() ? houseInfo[VillimKeys.KEY_HOUSE_PIC_URLS].arrayValue.map{ $0.stringValue } : []
     
-        let reservationArray = hosueInfo[VillimKeys.KEY_RESERVATIONS].array
+        let reservationArray = houseInfo[VillimKeys.KEY_HOUSE_ID].exists() ? houseInfo[VillimKeys.KEY_RESERVATIONS].array : nil
         if (reservationArray != nil) {
             self.reservations = [VillimReservation]()
             for reservationInfo in reservationArray! {
@@ -102,7 +102,7 @@ class VillimHouse {
         var houses : [VillimHouse] = []
         
         for jsonHouse in jsonHouses {
-            houses.append(VillimHouse(hosueInfo: jsonHouse))
+            houses.append(VillimHouse(houseInfo: jsonHouse))
         }
         
         return houses
