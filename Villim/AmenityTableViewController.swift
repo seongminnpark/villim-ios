87de1cb3a3653cd3a73497b4ad4710c6ea38691d
Seongmin Park <seongmin@Seongmins-iMac.local>
2017-08-04 08:00:38 -0400

    Fix amenity viewcontroller

--- Villim/AmenityTableViewController.swift
+++ Villim/AmenityTableViewController.swift
@@ -23,11 +23,13 @@ class AmenityTableViewController: UITableViewController {
         
         /* Initialize tableview */
         self.tableView = UITableView()
+        self.tableView.backgroundColor = VillimValues.backgroundColor
         self.tableView.delegate = self
         self.tableView.dataSource = self
         self.tableView.tableFooterView = UIView(frame: CGRect.zero) // Get rid of unnecessary cells stretching to the bottom.
         self.tableView.isScrollEnabled = true
         self.tableView.allowsSelection = false
+        self.tableView.showsVerticalScrollIndicator = false
         self.tableView.separatorInset = UIEdgeInsets.zero
     }
 
@@ -51,9 +53,16 @@ class AmenityTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "reuseIdentifier")
         cell.separatorInset = UIEdgeInsets.zero
+        cell.contentView.backgroundColor = VillimValues.backgroundColor
+        cell.textLabel?.font = UIFont(name: "NotoSansCJKkr-Regular", size: 17)
+        cell.textLabel?.textColor = UIColor(red:0.02, green:0.02, blue:0.04, alpha:1.0)
         cell.textLabel?.text = VillimAmenity.getAmenityName(amenityId: amenities[indexPath.row])
         cell.imageView?.image = VillimAmenity.getAmenityImage(amenityId: amenities[indexPath.row])
         return cell
     }
 
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
 }
--- Villim/AmenityViewController.swift
+++ Villim/AmenityViewController.swift
@@ -12,6 +12,8 @@ class AmenityViewController: UIViewController {
 
     var amenities = [Int]()
     
+    let sideMargin : CGFloat = 20.0
+    
     private var amenityTitle : UILabel!
     private var amenityTableViewController : AmenityTableViewController!
     
@@ -19,11 +21,15 @@ class AmenityViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = UIColor.white
-        self.title = ""
+        self.view.backgroundColor = VillimValues.backgroundColor
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.extendedLayoutIncludesOpaqueBars = true
+        self.navigationController?.navigationBar.barTintColor = VillimValues.backgroundColor
         
         /* Add Title */
         amenityTitle = UILabel()
+        amenityTitle.font = UIFont(name: "NotoSansCJKkr-Regular", size: 20)
+        amenityTitle.textColor = UIColor(red:0.02, green:0.05, blue:0.08, alpha:1.0)
         amenityTitle.text = NSLocalizedString("amenity_title", comment: "")
         self.view.addSubview(amenityTitle)
         
@@ -44,14 +50,15 @@ class AmenityViewController: UIViewController {
         
         /* Amenity title */
         amenityTitle?.snp.makeConstraints { (make) -> Void in
-            make.left.equalTo(self.view)
-            make.top.equalTo(self.view).offset(topOffset)
+            make.left.equalTo(self.view).offset(sideMargin)
+            make.top.equalTo(self.view).offset(topOffset + sideMargin * 0.3)
         }
         
         /* Tableview */
         amenityTableViewController.tableView.snp.makeConstraints{ (make) -> Void in
-            make.width.equalTo(self.view)
-            make.top.equalTo(amenityTitle.snp.bottom)
+            make.left.equalToSuperview().offset(sideMargin)
+            make.right.equalToSuperview().offset(-sideMargin)
+            make.top.equalTo(amenityTitle.snp.bottom).offset(sideMargin)
             make.bottom.equalTo(self.view)
         }
         
